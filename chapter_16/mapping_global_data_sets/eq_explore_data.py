1b63416e4f32902dc0f1b64065945c2dff3e3063
C.Y. Li <lcy@everyware.com.cn>
2022-05-28 03:33:25 +0800

    第一次提交：20220528

---
+++ chapter_16/mapping_global_data_sets/eq_explore_data.py
@@ -0,0 +1,23 @@
+import json
+
+filename = 'data/eq_data_30_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+all_eq_dicts = all_eq_data['features']
+mags, titles, lons, lats = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
+
+
+if __name__ == '__main__':
+    print(mags[:5])
+    print(titles[:5])
+    print(lons[:5])
+    print(lats[:5])
