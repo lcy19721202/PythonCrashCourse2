e478a680aa865a880c3ff6867e688e18007dc523
C.Y. Li <lcy@everyware.com.cn>
2022-06-01 18:01:47 +0800

    Commit Work for MAC Air

---
+++ Work/test_002.py
@@ -0,0 +1,17 @@
+#!/opt/local/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy  # numpy是用于科学计算的Python扩展库
+
+print(__name__)
+print("矩阵乘法演示")
+
+x = numpy.ones(3)  # ones()函数用于生成全1矩阵
+m = numpy.eye(3)  # eye()函数用于生成单位矩阵
+print("初始矩阵：\n", "\nx\n", x, "\nm\n", m)
+m = m * 3
+m[0, 2] = 5  # 设置矩阵指定位置上元素的值
+m[2, 0] = 3
+print("m*3:\n", m)
+y = x @ m  # 矩阵相乘
+print("x @ y\n", y)
---
+++ Work/test_003.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 3.8 旅行游戏
+
+print()
